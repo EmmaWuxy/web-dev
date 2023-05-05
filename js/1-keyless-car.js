4478be02b608cc30e1b66044e7ec009adcfcdec4
EmmaWuxy <xinyi.wu@mail.mcgill.ca>
2023-05-05 19:32:17 +0800

    improve keyless car code

--- js/1-keyless-car.js
+++ js/1-keyless-car.js
@@ -1,13 +1,17 @@
-// Ask for age
+// Function to check Driver's age
+function checkDriverAge(age){
+    
+    // if under 18, reject, equals 18, congrats, else power on
+    if (age < 18){
+    console.log("Sorry, you are too young to drive this car. Powering off");
+    }
+    else if(age === 18){
+    console.log( "Congratulations on your first year of driving. Enjoy the ride!");
+    }
+    else{
+    console.log("Powering On. Enjoy the ride!");
+    }
+}
+
 let age = Number(prompt("Your age is:"));
-// if under 18, reject, equals 18, congrats, else power on
-if (age < 18){
-    alert("Sorry, you are too young to drive this car. Powering off");
-}
-else if(age === 18){
-    alert( "Congratulations on your first year of driving. Enjoy the ride!");
-}
-else{
-    alert("Powering On. Enjoy the ride!"
-    );
-}
+checkDriverAge();
