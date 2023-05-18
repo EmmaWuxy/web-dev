d995124a66a2d6bfb0f28b83c0659aa02a797c33
EmmaWuxy <xinyi.wu@mail.mcgill.ca>
2023-05-18 19:23:23 +0800

    js facebook practice

---
+++ js/4-build-facebook/script.js
@@ -0,0 +1,24 @@
+let user = {
+    username: 'Emma',
+    password: '12345',
+};
+
+let database = [user];
+
+let newsfeed = [
+    {
+        username: 'Alex',
+        timeline: 'heyyy'
+    },
+    {
+        username: 'Bobby',
+        timeline: 'yoooo'
+    },
+    {
+        username: 'Seline',
+        timeline: 'hellooo'
+    }
+];
+
+let userNamePrompt = prompt("What's your username?");
+let passwordPrompt = prompt("What's your password?");
