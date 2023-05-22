ed56bc840719a7ca59c31b28cca7cee85a136bb3
EmmaWuxy <xinyi.wu@mail.mcgill.ca>
2023-05-22 19:07:38 +0800

    add random button to randomly generate color

--- js/6-color-generator/script.js
+++ js/6-color-generator/script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var btn = document.querySelector("button");
 
 console.log(body.style.background);
 
@@ -29,11 +30,23 @@ function rgbToHex(r, g, b){
 	return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
 }
 
+function generateRandomHex(){
+	return"#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+}
+
+function randomBgr(){
+	color1.value = generateRandomHex();
+	color2.value = generateRandomHex();
+	setGradient();
+}
+
 displayColorGradient();
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+btn.addEventListener("click", randomBgr)
 
 // Colour inputs match the background generated on the first page load
 var gradientRgbArr = getColorGradient().match(/[\d\.]+/g);
