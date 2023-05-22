4664fab6f54c9ac272c52b09f01c45388ebd7f5d
EmmaWuxy <xinyi.wu@mail.mcgill.ca>
2023-05-22 18:30:49 +0800

    Colour inputs match the background generated on the first page load

--- js/6-color-generator/script.js
+++ js/6-color-generator/script.js
@@ -17,8 +17,16 @@ function setGradient() {
 	displayColorGradient();
 }
 
+function getColorGradient(){
+	return getComputedStyle(body).getPropertyValue("background-image");
+}
+
 function displayColorGradient(){
-	css.textContent = getComputedStyle(body).getPropertyValue("background-image") + ";";
+	css.textContent = getColorGradient() + ";";
+}
+
+function rgbToHex(r, g, b){
+	return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
 }
 
 displayColorGradient();
@@ -28,4 +36,7 @@ color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 // Colour inputs match the background generated on the first page load
-//color1.setAttribute("value", body);
+var gradientRgbArr = getColorGradient().match(/[\d\.]+/g);
+console.log(gradientRgbArr);
+color1.setAttribute("value", rgbToHex(gradientRgbArr[0], gradientRgbArr[1], gradientRgbArr[2]));
+color2.setAttribute("value", rgbToHex(gradientRgbArr[3], gradientRgbArr[4], gradientRgbArr[5]));
