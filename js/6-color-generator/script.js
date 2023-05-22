e9005d67a082f3f2d126e93ae021fe8f328c15f8
EmmaWuxy <xinyi.wu@mail.mcgill.ca>
2023-05-22 18:31:24 +0800

    Colour inputs match the background generated on the first page load

--- js/6-color-generator/script.js
+++ js/6-color-generator/script.js
@@ -37,6 +37,5 @@ color2.addEventListener("input", setGradient);
 
 // Colour inputs match the background generated on the first page load
 var gradientRgbArr = getColorGradient().match(/[\d\.]+/g);
-console.log(gradientRgbArr);
 color1.setAttribute("value", rgbToHex(gradientRgbArr[0], gradientRgbArr[1], gradientRgbArr[2]));
 color2.setAttribute("value", rgbToHex(gradientRgbArr[3], gradientRgbArr[4], gradientRgbArr[5]));
